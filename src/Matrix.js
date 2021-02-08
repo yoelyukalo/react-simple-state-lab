3cb88029e5314e65989e634d621ab22b3a11ccdf
yoelyukalo <yoelyukalo@gmail.com>
2021-02-08 16:40:56 -0600

    defaultprop not working, done

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map(val => <Cell value={val} />)
   }
+  
   
   genMatrix = () => {
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
@@ -18,4 +19,11 @@ export default class Matrix extends Component {
     )
   }
   
+  Matrix.defaultProps = {
+    values: (() => {
+      const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+      return (new Array(10).fill(defRow))
+    })()
+  }
+
 }
